c98bfb67d5313f3956772bb976e281a166c882e5
godliang <2426534657@qq.com>
2021-07-07 20:09:21 +0800

    死信队列-消息TTL过期 队列达到最大长度

---
+++ rabbitmq-hello/src/main/java/com/atguigu/rabbitmq/eight/Consumer02.java
@@ -0,0 +1,34 @@
+package com.atguigu.rabbitmq.eight;
+
+import com.atguigu.rabbitmq.utils.RabbitMqUtils;
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 死信队列
+ * <p>
+ * 消费者1
+ */
+public class Consumer02 {
+
+
+    //死信队列的名称
+    public static final String DEAD_QUEUE = "dead_queue";
+
+    public static void main(String[] args) throws Exception {
+
+        Channel channel = RabbitMqUtils.getChannel();
+
+
+        DeliverCallback deliverCallback = (consumerTag, message) -> {
+            System.out.println("Consumer02接收的消息是：" + new String(message.getBody(), "UTF-8"));
+        };
+        channel.basicConsume(DEAD_QUEUE, true, deliverCallback, consumerTag -> {
+        });
+
+    }
+}
